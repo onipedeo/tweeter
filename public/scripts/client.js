dae2244ccfd57c5dc8395334364605477b8b3aa6
Tobi Onipede <onipede89@gmail.com>
2023-07-14 05:54:01 +0100

    implement ajax call to db to post new tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,31 +8,31 @@
 $(document).ready(function() {
 
   //Dummy tweet Object from the initial-tweet.json
-  const tweetArticle = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+  // const tweetArticle = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd"
+  //     },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ];
 
   //Function to createTweet Element dynamically and append to the tweet section
   const createTweetElement = (tweetObject) => {
@@ -70,12 +70,36 @@ $(document).ready(function() {
 
   // Function that appends tweets to the tweet container.
   const renderTweets = (tweetData) => {
+    $('.tweet-container').empty();
     tweetData.forEach((tweet) => {
       const $tweet = createTweetElement(tweet);
-      $('.tweet-container').append($tweet);
+      $('.tweet-container').prepend($tweet);
     });
   };
 
+  // renderTweets(tweetArticle);
 
-  renderTweets(tweetArticle);
+  $('.post-tweet').on("submit", function(event) {
+    event.preventDefault();
+    const data = $('.post-tweet').serialize();
+    $.post("/tweets", data).then(() => {
+      loadTweets();
+    });
+  });
+
+  const loadTweets = function() {
+
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      success: function(data) {
+        renderTweets(data);
+      },
+      error: function(err) {
+        console.log(err);
+      }
+    });
+
+  };
+
 });
