267127ebfaef6bc6f9534d8fc8be2e2f7d8e91f5
Tobi Onipede <onipede89@gmail.com>
2023-07-21 03:35:52 +0100

    indentation correction

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     const counter = $(this).parent().find('.counter');
     counter.text(maxlen - currentLen);
 
-    //checking if maxlen is more than currentlen 
+    //checking if maxlen is more than currentlen
     if (currentLen <= maxlen) {
       $(counter).removeClass('counterRed').addClass('counterBlack');
     } else {
